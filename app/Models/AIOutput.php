d4169a2017cac63ff9fac625abc6a55e429faf52
DWAI Dev <dev@dwai.studio>
2026-04-06 16:06:59 +0000

    Fix: set correct table name in AIOutput model

--- app/Models/AIOutput.php
+++ app/Models/AIOutput.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AIOutput extends Model
+    protected $table = 'ai_outputs';
 {
     use HasFactory;
 
